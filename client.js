4ab2095ef628953750f492f3dc93f96444aa1cbc
kyle <belfortk@gmail.com>
2017-11-23 14:04:15 -0800

    made websocket secure

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ var sharedb = require('sharedb/lib/client');
 var StringBinding = require('sharedb-string-binding');
 
 // Open WebSocket connection to ShareDB server
-var socket = new WebSocket('ws://' + window.location.host);
+var socket = new WebSocket('wss://' + window.location.host);
 var connection = new sharedb.Connection(socket);
 
 // Create local Doc instance mapped to 'examples' collection document with id 'textarea'
